d9c0e00966279e242e45d905942765a903df4e6d
Tiago <lazy.ufms@gmail.com>
2021-09-06 11:43:03 -0400

    Teste pokeapi

--- app.js
+++ app.js
@@ -1,5 +1,24 @@
 const button = document.querySelector('.submit')
+const name = document.querySelector('input')
 
 button.addEventListener('click', function() {
-  console.log('test')
+  requisicao();
 })
+
+const requisicao = async () => {
+  try {
+    const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/`)
+    {name, url} = {response.data.results.name, response.data.results.url}
+    console.log(name, url);
+/*     const namePoke = response.data
+    console.log(namePoke)
+    bulbasaur(namePoke); */
+  } catch(e) {
+    console.log(e);
+  }
+}
+
+function bulbasaur(response) {
+/*   */
+    console.log(name, url)
+}
